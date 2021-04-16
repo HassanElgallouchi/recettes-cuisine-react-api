84420d0c6cadd93909527d9bd4f0d631b10f6c8f
RebeccaKanu <kanurebecca0@gmail.com>
2021-04-16 12:17:35 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Favorite from "./views/Favorite/index";
-import Homepage from "./views/Homepage/";
+import Homepage from "./views/Homepage/index";
+import Header from './components/Header/index';
+import Footer from './components/Footer/index';
 
 
 function App() {
@@ -10,9 +12,10 @@ function App() {
   return (
 
     <Router>
-      
+      <Header/>
       <Route exact path="/" component={Homepage} />
       <Route exact path="/favorite" component={Favorite} />
+      <Footer/>
 
     </Router>
   );
--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
-import Header from '../../components/Header/'
 import RandomRecipe from '../../components/RandomRecipe/index.js'
 import RecipeCategories from '../../components/RecipeCategories'
+
+
 
 export default class Index extends Component {
     render() {
         return (
             <div>
-                <Header/>
+               
                 <RandomRecipe/>
                 <RecipeCategories/>
+              
             </div>
         )
     }
