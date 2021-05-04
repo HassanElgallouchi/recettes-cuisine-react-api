b4214129e3150e8a61a0c125079f05c6ca7ed7a6
RebeccaKanu <kanurebecca0@gmail.com>
2021-05-04 10:16:39 +0200

    final work

--- src/views/Categories/FoodCategorie.js
+++ src/views/Categories/FoodCategorie.js
@@ -1,60 +1,54 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import './FoodCategorie.css'
+import axios from "axios";
+import React, { Component } from "react";
+import "./FoodCategorie.css";
 
 export default class FoodCategorie extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            data: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
 
-    async componentDidMount (){
-        const categorie = this.props.match.params.category;
+  async componentDidMount() {
+    const categorie = this.props.match.params.category;
 
-        const response = await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=' + categorie)
+    const response = await axios.get(
+      "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + categorie
+    );
 
-        console.log('YO!!!',categorie);
+    console.log("YO!!!", categorie);
 
-        this.setState({
-            data: response.data.meals
-        })
-        console.log(this.state.data.meals);
-    }
-    
-    render() {
-        return (
-            <div>
-            <div className='titleCateRcp'>
-              <h1>Recipes</h1>
-            </div>
-            <div class='recipeParent'>
-              {this.state.data.map((e, index) => {
-                  return (
-                    
- 
-                     <div key={index} className='recip1'>
+    this.setState({
+      data: response.data.meals,
+    });
+    console.log(this.state.data.meals);
+  }
 
-                       <a href={'/recette/' + e.idMeal}>
-                         <h1>{e.strMeal}</h1>
-                       </a>
+  render() {
+    return (
+      <div>
+        <div className="titleCateRcp">
+          <h1>Recipes</h1>
+        </div>
+        <div className="recipeParent">
+          {this.state.data.map((e, index) => {
+            return (
+              <div key={index} className="recip1">
+                <a href={"/recette/" + e.idMeal}>
+                  <h1>{e.strMeal}</h1>
+                </a>
 
-                       <div className='recipeCImg'>
-                         <a href={'/recette/' + e.idMeal}><img src={e.strMealThumb} className='imgC'/></a>
-                       </div>
-
-                     </div>
-                 
-
-                   
-                  )
-              })}
-            </div>
-
-
-                
-            </div>
-        )
-    }
+                <div className="recipeCImg">
+                  <a href={"/recette/" + e.idMeal}>
+                    <img src={e.strMealThumb} className="imgC" />
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
